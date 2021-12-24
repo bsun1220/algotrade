c66e24a75ec742072ab0ce6971dea5e6e61f1c31
bsun1220 <65567539+bsun1220@users.noreply.github.com>
2021-12-24 14:43:02 -0500

    Add files via upload

--- algotrade.py
+++ algotrade.py
@@ -3,63 +3,97 @@ import pandas as pd
 import numpy as np
 
 
-class MeanReversionAlgorithmFramework(QCAlgorithm):
+class BuyOnGapModel(QCAlgorithm):
 
     def Initialize(self):
-        self.SetStartDate(2020, 12, 1)  # Set Start Date
-        self.SetEndDate(2021, 12, 15)  # Set Start Date
-        self.SetCash(100000)  # Set Strategy Cash
+        self.SetStartDate(2020, 12, 1)
+        self.SetEndDate(2021, 12, 1)
+        self.SetCash(100000)
+        self.AddEquity("SPY", Resolution.Minute)
 
         self.UniverseSettings.Resolution = Resolution.Daily
         self.AddUniverse(self.CoarseFilter, self.FineFilter)
-        self.AddEquity("SPY", Resolution.Minute)
-
         self.universe = None
 
-        self.long_leverage = 1.0
-        self.short_leverage = -1.0
+        self.long_leverage = 0.9
+        self.short_leverage = -0.9
 
         self.rebalance_universe = True
-        self.first_month = True
 
-        self.Schedule.On(self.DateRules.MonthStart("SPY"), self.TimeRules.At(0, 0), Action(self.Monthly_Rebalance))
-        self.Schedule.On(self.DateRules.MonthStart("SPY"), self.TimeRules.At(0, 0), Action(self.set_leverage))
-        self.Schedule.On(self.DateRules.EveryDay("SPY"), self.TimeRules.BeforeMarketClose("SPY", 303),
-                         Action(self.get_prices))
-        self.Schedule.On(self.DateRules.EveryDay("SPY"), self.TimeRules.BeforeMarketClose("SPY", 302),
-                         Action(self.daily_rebalance))
-        self.Schedule.On(self.DateRules.EveryDay("SPY"), self.TimeRules.BeforeMarketClose("SPY", 301),
-                         Action(self.short))
-        self.Schedule.On(self.DateRules.EveryDay("SPY"), self.TimeRules.BeforeMarketClose("SPY", 300),
-                         Action(self.long))
+        self.longs = []
+        self.shorts = []
+
+        self.Schedule.On(self.DateRules.MonthStart("SPY"), self.TimeRules.At(0, 0), Action(self.UniverseRebalance))
+        self.Schedule.On(self.DateRules.MonthStart("SPY"), self.TimeRules.At(0, 0), Action(self.SetLeverage))
+        self.Schedule.On(self.DateRules.EveryDay("SPY"), self.TimeRules.AfterMarketOpen("SPY", 6),
+                         Action(self.AlphaIndicator))
+        self.Schedule.On(self.DateRules.EveryDay("SPY"), self.TimeRules.AfterMarketOpen("SPY", 7), Action(self.Long))
+        self.Schedule.On(self.DateRules.EveryDay("SPY"), self.TimeRules.AfterMarketOpen("SPY", 8), Action(self.Short))
+        self.Schedule.On(self.DateRules.EveryDay("SPY"), self.TimeRules.BeforeMarketClose("SPY", 5), Action(self.Close))
 
     def OnData(self, data):
         pass
 
-    def Monthly_Rebalance(self):
-        self.rebalance_universe = True
-
     def CoarseFilter(self, coarse):
-        if self.rebalance_universe or self.first_month:
+        if self.rebalance_universe:
             sortedByDollarVolume = sorted(coarse, key=lambda x:
             x.DollarVolume, reverse=True)
 
-            filtered = [x.Symbol for x in sortedByDollarVolume
-                        if float(x.Price) > 5 and
-                        x.HasFundamentalData]
-            return filtered[:50]
+            filtered = [x.Symbol for x in sortedByDollarVolume if
+                        x.HasFundamentalData and
+                        float(x.Price) > 5]
+
+            return filtered[:200]
         else:
             return self.universe
 
     def FineFilter(self, fine):
-        if self.rebalance_universe or self.first_month:
-            filtered = [x for x in fine if x.ValuationRatios.EVToEBITDA > 0]
-            self.universe = [x.Symbol for x in filtered]
+        if self.rebalance_universe:
+            filtered = [x for x in fine if
+                        x.ValuationRatios.EVToEBITDA > 0]
+            self.universe = [x.Symbol for x in fine]
             self.rebalance_universe = False
-            self.first_month = False
         return self.universe
 
-    def set_leverage(self):
+    def UniverseRebalance(self):
+        self.rebalance_universe = True
+
+    def AlphaIndicator(self):
+        if self.universe is None: return
+
+        shortlist = []
+        longlist = []
+
+        history = self.History(self.universe, 90, Resolution.Daily)
+
+        for i in self.universe:
+            if str(i) in history.index.levels[0]:
+                close_90 = history.loc[str(i)]['close']
+                stock_ret = np.log(close_90 / close_90.shift(1))
+                mean_ret = stock_ret.mean()
+                std_ret = stock_ret.std()
+
+                open = history.loc[str(i)]['open'][-1]
+                low = history.loc[str(i)]['low'][-2]
+                current_ret = np.log(open / low)
+
+                if current_ret < mean_ret - std_ret:
+                    ma_20 = close_90[-20:].mean()
+                    if open > ma_20:
+                        longlist.append(i)
+
+                elif current_ret > mean_ret + std_ret:
+                    ma_20 = close_90[-20:].mean()
+                    if open < ma_20:
+                        shortlist.append(i)
+
+        self.longs = longlist
+        self.shorts = shortlist
+
+    def PortfolioWeightings(self):
+        pass
+
+    def SetLeverage(self):
         pri = self.History(["SPY"], 200, Resolution.Daily)
         pos_one = (pri.loc["SPY"]['close'][-1])
         pos_six = (pri.loc["SPY"]['close'][-75:].mean())
@@ -71,55 +105,28 @@ class MeanReversionAlgorithmFramework(QCAlgorithm):
             self.long_leverage = 1.1
             self.short_leverage = -0.7
 
-    def long(self):
-        if (self.universe is None) or (len(self.longlist) + self.existinglong == 0): return
-
-        for symbol in self.longlist:
-            self.AddEquity(symbol, Resolution.Minute)
-            self.SetHoldings(symbol, self.long_leverage / (len(self.longlist) + self.existinglong))
-
-    def short(self):
-        if (self.universe is None) or (len(self.shortlist) + self.existingshort == 0): return
-
-        for symbol in self.shortlist:
-            self.AddEquity(symbol, Resolution.Minute)
-            self.SetHoldings(symbol, self.short_leverage / (len(self.shortlist) + self.existingshort))
-
-    def get_prices(self):
-        if self.universe is None: return
-
-        prices = {}
-        hist = self.History(self.universe, 6, Resolution.Daily)
-        for i in self.universe:
-            if str(i) in hist.index.levels[0]:
-                prices[i.Value] = hist.loc[str(i)]['close']
-        df_prices = pd.DataFrame(prices, columns=prices.keys())
-
-        daily_rets = np.log(df_prices / df_prices.shift(1))
-        rets = (df_prices.iloc[-2] - df_prices.iloc[0]) / df_prices.iloc[0]
-        stdevs = daily_rets.std(axis=0)
-
-        self.ret_qt = pd.qcut(rets, 5, labels=False) + 1
-        self.stdev_qt = pd.qcut(stdevs, 3, labels=False) + 1
-        self.longlist = list((self.ret_qt[self.ret_qt == 1].index) & (self.stdev_qt[self.stdev_qt < 3].index))
-        self.shortlist = list((self.ret_qt[self.ret_qt == 5].index) & (self.stdev_qt[self.stdev_qt < 3].index))
-
-    def daily_rebalance(self):
-        if self.universe is None: return
-        self.existingshort = 0
-        self.existinglong = 0
+    def Close(self):
+        if self.universe is None:
+            return
 
         for symbol in self.Portfolio.Keys:
-            if (symbol.Value != 'SPY') and (symbol.Value in self.ret_qt.index):
-                current_quantile = self.ret_qt.loc[symbol.Value]
-                if self.Portfolio[symbol].Quantity > 0:
-                    if (current_quantile == 1) and (symbol not in self.longlist):
-                        self.existinglong += 1
-                    elif (current_quantile > 1) and (symbol not in self.shortlist):
-                        self.SetHoldings(symbol, 0)
-                elif self.Portfolio[symbol].Quantity < 0:
-                    if (current_quantile == 5) and (symbol not in self.shortlist):
-                        self.existingshort += 1
-                    elif (current_quantile < 5) and (symbol not in self.longlist):
-                        self.SetHoldings(symbol, 0)
+            if (symbol in self.longs or symbol in self.shorts):
+                self.SetHoldings(symbol, 0)
+
+    def Long(self):
+        if (self.universe is None) or len(self.longs) == 0: return
+
+        for symbol in self.longs:
+            self.AddEquity(symbol, Resolution.Minute)
+            self.SetHoldings(symbol, self.long_leverage / len(self.longs))
+
+    def Short(self):
+        if (self.universe is None) or len(self.shorts) == 0: return
+
+        for symbol in self.shorts:
+            self.AddEquity(symbol, Resolution.Minute)
+            self.SetHoldings(symbol, self.short_leverage / len(self.shorts))
+
+    def RiskManagement(self):
+        pass
 
